b65d87a0bc476d83ac664900024932a4eb2ea734
yarenunal <yareunal16@gmail.com>
2025-03-22 02:53:09 +0300

    filtre

---
+++ eTicaret/Views/filterView.swift
@@ -0,0 +1,18 @@
+//
+//  filterView.swift
+//  eTicaret
+//
+//  Created by Yaren on 22.03.2025.
+//
+
+import SwiftUI
+
+struct filterView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    filterView()
+}
